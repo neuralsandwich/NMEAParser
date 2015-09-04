85cd49a65929c8354400398869f9c1bf7f6a75d7
Sean Jones <neuralsandwich@gmail.com>
2015-09-04 22:01:25 +0100

    Add ParseFloat_test.cpp

---
+++ test/ParseFloat_Test.cpp
@@ -0,0 +1,26 @@
+#include "NMEAParser.cpp"
+#include "gtest/gtest.h"
+
+#include <cmath>
+
+namespace NMEA {
+TEST(ParseFloat, Valid_Float) {
+  const std::string String = "3.0";
+  const float Expected = 3.0f;
+
+  EXPECT_EQ(Expected, ParseFloat(String));
+}
+
+TEST(ParseFloat, Invalid_Float) {
+  const std::string String = "ASDF";
+
+  EXPECT_TRUE(isnan(ParseFloat(String)));
+}
+
+TEST(ParseFloat, Invalid_Empty_Float) {
+  const std::string String = "";
+  const float Expected = NAN;
+
+  EXPECT_TRUE(isnan(ParseFloat(String)));
+}
+}
