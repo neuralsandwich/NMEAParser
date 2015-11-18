19a55178afd9df4bf69be156a15bdd0c7b78d256
Sean Jones <neuralsandwich@gmail.com>
2015-11-18 02:33:37 +0000

    THSMessageParse, don't alloc message on heap

--- test/THSMessageParse_Test.cpp
+++ test/THSMessageParse_Test.cpp
@@ -17,20 +17,17 @@ namespace NMEA {
 TEST(THSMessageParse, Valid_Message) {
   const std::string RawMessage{"$GPTHS,77.52,E*34"};
 
-  NMEAHeader Header{
-      .ID = NMEA_TALKER_ID::GPS, .Type = NMEA_MESSAGE_TYPE::THS, .Valid = 1};
   GPTHS Message{77.52f, 'E'};
-  NMEAMessage Expected{.Header = &Header, .THS = &Message};
+  NMEAMessage Expected{NMEA_TALKER_ID::GPS, NMEA_MESSAGE_TYPE::THS, 1,
+                       .THS = &Message};
 
   auto Parser = NMEAParser{};
   auto Result = Parser.Parse(RawMessage);
 
   // Compare Headers
-  EXPECT_EQ(Expected.Header->ID, Result->Header->ID)
-      << "Talker ID is incorrect";
-  EXPECT_EQ(Expected.Header->Type, Result->Header->Type)
-      << "Message type is incorrect";
-  EXPECT_EQ(Expected.Header->Valid, Result->Header->Valid)
+  EXPECT_EQ(Expected.ID, Result->ID) << "Talker ID is incorrect";
+  EXPECT_EQ(Expected.Type, Result->Type) << "Message type is incorrect";
+  EXPECT_EQ(Expected.Valid, Result->Valid)
       << "Message valid status is incorrect";
 
   EXPECT_FLOAT_EQ(Expected.THS->headt, Result->THS->headt);
@@ -40,42 +37,36 @@ TEST(THSMessageParse, Valid_Message) {
 TEST(THSMessageParse, Invalid_Message) {
   const std::string RawMessage{"*ASDflaksdjflkasjdflaksdjf"};
 
-  NMEAHeader Header = {.ID = NMEA_TALKER_ID::UNKNOWN_TALKER_ID,
-                       .Type = NMEA_MESSAGE_TYPE::UNKNOWN_MESSAGE,
-                       .Valid = 0};
-
-  NMEAMessage Expected = {.Header = &Header, {}};
+  NMEAMessage Expected = {NMEA_TALKER_ID::UNKNOWN_TALKER_ID,
+                          NMEA_MESSAGE_TYPE::UNKNOWN_MESSAGE,
+                          0,
+                          {}};
 
   auto Parser = NMEAParser{};
   auto Result = Parser.Parse(RawMessage);
 
   // Compare Headers
-  EXPECT_EQ(Expected.Header->ID, Result->Header->ID)
-      << "Talker ID is incorrect";
-  EXPECT_EQ(Expected.Header->Type, Result->Header->Type)
-      << "Message type is incorrect";
-  EXPECT_EQ(Expected.Header->Valid, Result->Header->Valid)
+  EXPECT_EQ(Expected.ID, Result->ID) << "Talker ID is incorrect";
+  EXPECT_EQ(Expected.Type, Result->Type) << "Message type is incorrect";
+  EXPECT_EQ(Expected.Valid, Result->Valid)
       << "Message valid status is incorrect";
 }
 
 TEST(THSMessageParse, Empty_Message) {
   const std::string RawMessage{""};
 
-  NMEAHeader Header = {.ID = NMEA_TALKER_ID::UNKNOWN_TALKER_ID,
-                       .Type = NMEA_MESSAGE_TYPE::UNKNOWN_MESSAGE,
-                       .Valid = 0};
-
-  NMEAMessage Expected = {.Header = &Header, {}};
+  NMEAMessage Expected = {NMEA_TALKER_ID::UNKNOWN_TALKER_ID,
+                          NMEA_MESSAGE_TYPE::UNKNOWN_MESSAGE,
+                          0,
+                          {}};
 
   auto Parser = NMEAParser{};
   auto Result = Parser.Parse(RawMessage);
 
   // Compare Headers
-  EXPECT_EQ(Expected.Header->ID, Result->Header->ID)
-      << "Talker ID is incorrect";
-  EXPECT_EQ(Expected.Header->Type, Result->Header->Type)
-      << "Message type is incorrect";
-  EXPECT_EQ(Expected.Header->Valid, Result->Header->Valid)
+  EXPECT_EQ(Expected.ID, Result->ID) << "Talker ID is incorrect";
+  EXPECT_EQ(Expected.Type, Result->Type) << "Message type is incorrect";
+  EXPECT_EQ(Expected.Valid, Result->Valid)
       << "Message valid status is incorrect";
 }
 }
